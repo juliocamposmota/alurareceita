575b10d6f7ab8eaa4e91aefec055bd196055fa68
Júlio Mota <juliomotacampos@gmail.com>
2021-08-19 10:34:58 -0300

    Cria requisicao POST para login e Cria pagina de Dashboard

--- usuarios/views.py
+++ usuarios/views.py
@@ -41,10 +41,20 @@ def cadastro(request):
     return render(request, 'usuarios/cadastro.html')
 
 def login(request):
+  if request.method == 'POST':
+    email = request.POST['email']
+    senha = request.POST['senha']
+
+    if email == '' or senha == '':
+      print('Email e/ ou senha inválidos. Tente novamente :)')
+      return redirect('login')
+
+    print(email, senha)
+    return redirect('dashboard')
   return render(request, 'usuarios/login.html')
 
 def logout(request):
   pass
 
 def dashboard(request):
-  pass
+  return render(request, 'usuarios/dashboard.html')
